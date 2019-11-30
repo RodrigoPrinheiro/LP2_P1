f65d1e166ae223e193c511d950816dc908432bce
Thomas the Franque <tfranque@gmail.com>
2019-11-30 19:47:14 +0000

    Add Class to Read files and Class to parse them

---
+++ IMDBDatabase/DataReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace IMDBDatabase
+{
+	class DataReader
+	{
+		/* 
+		 * ####################################################################
+		 *							###	TO-DO ###
+		 * - Try parse for when file not found
+		 * - Try parse for when file could not be uncompressed
+		 * ####################################################################
+		*/
+
+		public void ReadData(string path, out string outDataString)
+		{
+			Console.WriteLine($"Opening {path}...");
+			using (Stream file = File.OpenRead(path))
+			{
+				Console.WriteLine("Successfully opened!");
+				outDataString = UncompressGZip(file);
+			}
+		}
+
+		private string UncompressGZip (Stream fileStream)
+		{
+			Console.WriteLine("Decompressing file...");
+
+			string finalOutput = "";
+
+			using (Stream gz =
+				new GZipStream(fileStream, CompressionMode.Decompress))
+			{
+				using (MemoryStream ms = new MemoryStream())
+				{
+					gz.CopyTo(ms);
+					finalOutput = Encoding.UTF8.GetString(ms.ToArray());
+
+					ms.Close();
+					gz.Close();
+
+					Console.WriteLine("Successfully decompressed!\n");
+					return finalOutput;
+				}
+			}
+		}
+	}
+}
--- IMDBDatabase/Program.cs
+++ IMDBDatabase/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading;
 
 namespace IMDBDatabase
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ThomasDebug(args);
+		}
+
+		private static void ThomasDebug(string[] args)
+		{
+			string path = Environment.GetFolderPath(
+				Environment.SpecialFolder.LocalApplicationData) +
+				"\\MyIMDBSearcher\\";
+
+			string fileName = "name.basics.tsv.gz";
+
+			string finalPath = path + fileName;
+
+			string dataNames = "";
+
+			DataReader dr = new DataReader();
+
+			dr.ReadData(finalPath, out dataNames);
+
+			DataParser.Parse(dataNames);
+		}
+	}
 }
