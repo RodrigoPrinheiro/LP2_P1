049a6551b449719be5f10741803e91b50795f2e0
Thomas the Franque <tfranque@gmail.com>
2019-12-01 15:10:46 +0000

    Add rating struct and refinement

--- IMDBDatabase/DataReader.cs
+++ IMDBDatabase/DataReader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Text;
+using System.Threading;
 
 namespace IMDBDatabase
 {
@@ -19,7 +20,7 @@ namespace IMDBDatabase
 		 * ####################################################################
 		*/
 
-		// tconst	titleType	primaryTitle	originalTitle	isAdult	startYear	endYear	runtimeMinutes	genres
+		// tconst	titleType	primaryTitle	originalTitle	isAdult	startYear	endYear		runtimeMinutes	genres
 
 		private const string _NAME_BASICS_FILENAME =	"name.basics.tsv.gz";
 		private const string _TITLE_AKAS_FILENAME =		"title.akas.tsv.gz";
@@ -29,15 +30,13 @@ namespace IMDBDatabase
 		private const string _TITLE_PRINCIPALS_FILENAME = "title.principals.tsv.gz";
 		private const string _TITLE_RATINGS_FILENAME =	"title.ratings.tsv.gz";
 
-		private Dictionary<int, string[]> titles;
 		private readonly string _path;
+		private string[] _titleRatings;
 
 
 		public DataReader()
 		{
 			Console.OutputEncoding = Encoding.UTF8;
-
-			titles = new Dictionary<int, string[]>();
 
 			_path = Environment.GetFolderPath(
 				Environment.SpecialFolder.LocalApplicationData) +
@@ -49,28 +48,43 @@ namespace IMDBDatabase
 
 		public void ReadData()
 		{
+			ProcessTitleBasicInfo();
+		}
+
+		private void GetRatingInfo()
+		{
+
+		}
+
+		private void ProcessTitleBasicInfo()
+		{
 			string titleLine;
 
-			Console.WriteLine($"Opening {_path}...");
+			WriteFakeLoadingDots("Searching for firewall breach");
 			using (FileStream fs = new FileStream(
 				_path, FileMode.Open, FileAccess.Read))
 			{
-				Console.WriteLine("Successfully opened!");
-				Console.WriteLine("Decompressing file...");
+				Console.WriteLine("Firewall breached.");
+				Thread.Sleep(400);
+				Console.Clear();
+				WriteFakeLoadingDots("Decrypting passwords");
 				using (GZipStream gzs = new GZipStream(
 					fs, CompressionMode.Decompress))
 				{
-					Console.WriteLine("Successfully decompressed!\n");
-					Console.WriteLine("Parsing file...");
+					Console.WriteLine("Passwords decrypted.\n");
+					Thread.Sleep(400);
+					Console.Clear();
+					WriteFakeLoadingDots("Hacking IMDB database");
 					using (StreamReader sr = new StreamReader(gzs, Encoding.UTF8))
 					{
 						while ((titleLine = sr.ReadLine()) != null)
 						{
 							// Parse line
 							ParseTitleBasicsLine(titleLine);
-							Console.ReadKey(true);
+							//Console.ReadKey(true);
 						}
-						Console.WriteLine("Successfully parsed!\n");
+						Console.WriteLine("Access Granted.\n");
+						Thread.Sleep(400);
 					}
 				}
 			}
@@ -79,25 +93,77 @@ namespace IMDBDatabase
 		private void ParseTitleBasicsLine(string line)
 		{
 			string[] words = line.Split('\t');
+			if (!words[0].StartsWith("tt")) return;
 
-			int titleID = default;
+			int    id =			default;
+			bool   isAdult =	default;
+			string type =		default;
+			string name =		default;
+			string startYear =	default;
+			string endYear =	default;
+			string genres =		default;
 
 			for (byte i = 0; i < words.Length; i++)
 			{
-				// Check if line has ID
-				if (words[i].Substring(0, 2) == "tt")
-					switch (i)
-					{
-						case 0:
-							titleID = int.Parse(words[i].Substring(2));
-							break;
-						case 1:
-							titleID = int.Parse(words[i].Substring(2));
-							break;
-					}
+				switch (i) 
+				{ 
+					// Title ID
+					case 0:
+						id = int.Parse(words[i].Substring(2));
+						break;
+					// Title Type
+					case 1:
+						type = words[i];
+						break;
+					// Title Primary Title
+					case 2:
+						name = words[i];
+						break;
+					// Title Original Title
+					case 3:
+						//titleName = words[i];
+						break;
+					// Title Is Adult
+					case 4:
+						isAdult = words[i] == "1" ? true : false;
+						break;
+					// Title Start Year
+					case 5:
+						startYear = words[i];
+						break;
+					// Title End Year
+					case 6:
+						endYear = words[i];
+						break;
+					// Title Runtime (min)
+					case 7:
+						break;
+					// Title Genres
+					case 8:
+						genres = words[i];
+						break;
+				}
 			}
 
-			Console.WriteLine();
+			//titles.Add(new Title(
+			//	id, 0, 0, name, type, genres, isAdult, startYear, endYear));
+		}
+
+		private Tuple<int, float> GetScoresFromID(int id)
+		{
+
+		}
+
+		private void WriteFakeLoadingDots(string fakeProcess)
+		{
+			Console.Write(fakeProcess);
+			Thread.Sleep(431);
+			Console.Write(".");
+			Thread.Sleep(327);
+			Console.Write(".");
+			Thread.Sleep(398);
+			Console.WriteLine(".");
+			Thread.Sleep(401);
 		}
 	}
 }
