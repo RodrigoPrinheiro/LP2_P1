cb44c7b359eda5e4c04c3a312bfd79b03545dd12
Thomas the Franque <tfranque@gmail.com>
2019-12-02 23:24:53 +0000

    Major performance improvements on file reading

--- IMDBDatabase/DataReader.cs
+++ IMDBDatabase/DataReader.cs
@@ -6,11 +6,11 @@
 /// @date 2019
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Text;
+using System.Collections.Generic;
 using System.Threading;
+using System.Text;
 
 namespace IMDBDatabase
 {
@@ -53,14 +53,26 @@ namespace IMDBDatabase
 		/// Path to local appdata
 		/// </summary>
 		private readonly string _path;
+
+		/// <summary>
+		/// Ammount of titles
+		/// </summary>
+		int _titleAmmount;
+
 		/// <summary>
 		/// Ui rendering
 		/// </summary>
 		private IInterface _ui;
+
 		/// <summary>
 		/// Ratings dictionary using title ID as key
 		/// </summary>
 		private Dictionary<int, Rating> _ratingDict;
+
+		/// <summary>
+		/// List containing titles
+		/// </summary>
+		private List<Title> _titleInfo;
 
 		/// <summary>
 		/// Constructor that initializes instance variables.
@@ -73,18 +85,33 @@ namespace IMDBDatabase
 			_path = Environment.GetFolderPath(
 				Environment.SpecialFolder.LocalApplicationData) +
 				"\\MyIMDBSearcher\\";
+			_titleAmmount = 0;
 		}
 
 		/// <summary>
 		/// Begin reading, decrypting and assigning data.
 		/// </summary>
-		public void ReadData()
+		public ICollection<Title> ReadData()
 		{
 			// Start data process
 			try
 			{
-				GetRatingInfo();
-				ProcessTitleBasicInfo();
+				// Get ratings
+				ReadFromFile(_TITLE_RATINGS_FILENAME, AddRatingTodictionary);
+
+				// Get the ammount of titles
+				ReadFromFile(
+					_TITLE_BASICS_FILENAME, IncrementAmmountOfTitleLines);
+				_titleAmmount++;
+
+				// Initialize title list with predetermined ammount of titles
+				_titleInfo = new List<Title>(_titleAmmount);
+
+				// Get titles into list
+				ReadFromFile(_TITLE_BASICS_FILENAME, AddTitleBasicsLineToList);
+
+				// Return fill title info
+				return _titleInfo;
 			} 
 			// If a file is not found
 			catch (FileNotFoundException e)
@@ -92,46 +119,59 @@ namespace IMDBDatabase
 				_ui.RenderError("\nOops, we couldn't find your file...");
 				throw e;
 			}
-		}
-
-		/// <summary>
-		/// Gets the raw lines of text of the rating info
-		/// from _TITLE_RATINGS_FILENAME and adds them to a Dictionary.
-		/// </summary>
-		private void GetRatingInfo()
-		{
-			string pathToFile = _path + _TITLE_RATINGS_FILENAME;
-			string titleLine;
-
-			_ui.ShowFakeLoadingProcess("Booting bootleg_unity-v.2021.TfRp");
-			// Open file
-			using (FileStream fs = new FileStream(
-				pathToFile, FileMode.Open, FileAccess.Read))
+			// If a file is not found
+			catch (OutOfMemoryException e)
 			{
-				_ui.ShowMsg("definetly-unity-v.2021.TfRp booted.", true);
-				Thread.Sleep(400);
+				_ui.RenderError("\nOops, memory run out... Thats our fault. :)");
+				throw e;
+			}
+
+			finally
+			{
+				_ui.ShowMsg($"\nList size is: {_titleAmmount}", true);
+				_ui.ShowMsg("\nFile reading successfull.\n", true);
+			}
+		}
+		
+		/// <summary>
+		/// Opens, uncompresses and reads a file, then make an action for every
+		/// string line on the read file.
+		/// </summary>
+		/// <param name="fileName">Name of the file to be read.</param>
+		/// <param name="lineAction">Action to be made per line.</param>
+		private void ReadFromFile(string fileName, Action<string> lineAction)
+		{
+			string titleLine = default;
+			string path = _path + fileName;
+
+			// Open file
+			_ui.ShowFakeLoadingProcess("Opening file: " + fileName);
+			using (FileStream fs = new FileStream(
+				path, FileMode.Open, FileAccess.Read))
+			{
+				_ui.ShowMsg("File found and opened.", true);
+				Thread.Sleep(200);
 				Console.Clear();
-				_ui.ShowFakeLoadingProcess("Spoofing");
 				// Uncompress file
+				_ui.ShowFakeLoadingProcess("Uncompressing");
 				using (GZipStream gzs = new GZipStream(
 					fs, CompressionMode.Decompress))
 				{
-					_ui.ShowMsg("Spoofing complete.\n", true);
-					Thread.Sleep(400);
+					_ui.ShowMsg("Uncompressing successfull.\n\n", true);
+					Thread.Sleep(200);
 					Console.Clear();
-					_ui.ShowFakeLoadingProcess("Searching for Honeypot");
 					// Read file
+					_ui.ShowFakeLoadingProcess("Reading data");
 					using (StreamReader sr = new StreamReader(gzs, Encoding.UTF8))
 					{
 						// Run through all of the raw text data
 						while ((titleLine = sr.ReadLine()) != null)
 						{
-							// Parse line
-							AddRatingTodictionary(titleLine);
+							// Invoke action
+							lineAction.Invoke(titleLine);
 						}
-						_ui.ShowMsg("Honeypot successfully found and avoided.\n",
-							true);
-						Thread.Sleep(400);
+						_ui.ShowMsg("Data read.\n", true);
+						Thread.Sleep(200);
 						Console.Clear();
 					}
 				}
@@ -139,58 +179,32 @@ namespace IMDBDatabase
 		}
 
 		/// <summary>
-		/// Processes raw text data from TITLE_BASICS_FILENAME
+		/// Increment ammount of title lines if the line isn't a header. 
 		/// </summary>
-		private void ProcessTitleBasicInfo()
-		{
-			string pathToFile = _path + _TITLE_BASICS_FILENAME;
-			string titleLine;
-
-			_ui.ShowFakeLoadingProcess("Searching for firewall breach");
-			// Open file
-			using (FileStream fs = new FileStream(
-				pathToFile, FileMode.Open, FileAccess.Read))
-			{
-				_ui.ShowMsg("Firewall breached.", true);
-				Thread.Sleep(400);
-				Console.Clear();
-				_ui.ShowFakeLoadingProcess("Decrypting passwords");
-				// Uncompress file
-				using (GZipStream gzs = new GZipStream(
-					fs, CompressionMode.Decompress))
-				{
-					_ui.ShowMsg("Passwords decrypted.\n", true);
-					Thread.Sleep(400);
-					Console.Clear();
-					_ui.ShowFakeLoadingProcess("Breaching IMDB database");
-					// Read file
-					using (StreamReader sr = new StreamReader(gzs, Encoding.UTF8))
-					{
-						// Run through all of the raw text data
-						while ((titleLine = sr.ReadLine()) != null)
-						{
-							// Parse line
-							ParseTitleBasicsLine(titleLine);
-						}
-						_ui.ShowMsg("Access Granted.\n", true);
-						Thread.Sleep(400);
-					}
-				}
-			}
-		}
-
-		/// <summary>
-		/// Parse raw title basics line data into a Title class.
-		/// </summary>
-		/// <param name="rawLine">Raw line data</param>
-		private void ParseTitleBasicsLine(string rawLine)
+		/// <param name="rawLine">Raw line.</param>
+		private void IncrementAmmountOfTitleLines(string rawLine)
 		{
 			// Split into words
 			string[] words = rawLine.Split('\t');
 			// Check if it is not a header
 			if (!words[0].StartsWith("tt")) return;
 
-			int    id =			default;
+			_titleAmmount++;
+		}
+
+		/// <summary>
+		/// Parse raw title basics line data into the Title Info collection.
+		/// </summary>
+		/// <param name="rawLine">Raw line info.</param>
+		/// <returns>The parsed title.</returns>
+		private void AddTitleBasicsLineToList(string rawLine)
+		{
+			// Split into words
+			string[] words = rawLine.Split('\t');
+			// Check if it is not a header
+			if (!words[0].StartsWith("tt")) return;
+
+			int id =			default;
 			bool   isAdult =	default;
 			string type =		default;
 			string name =		default;
@@ -215,10 +229,6 @@ namespace IMDBDatabase
 					case 2:
 						name = words[i];
 						break;
-					// Title Original Title
-					case 3:
-						//titleName = words[i];
-						break;
 					// Title Is Adult
 					case 4:
 						isAdult = words[i] == "1" ? true : false;
@@ -231,9 +241,6 @@ namespace IMDBDatabase
 					case 6:
 						endYear = words[i];
 						break;
-					// Title Runtime (min)
-					case 7:
-						break;
 					// Title Genres
 					case 8:
 						genres = words[i];
@@ -241,17 +248,18 @@ namespace IMDBDatabase
 				}
 			}
 
-			// Pass the info
+			Rating rating = GetRatingFromID(id);
 
-			//titles.Add(new Title(
-			//	id, GetRatingFromId(id), name, type, genres, 
-			//		isAdult, startYear, endYear));
+			// Pass the info
+			_titleInfo.Add(new Title(
+				id, rating, name, type, genres,
+					isAdult, startYear, endYear));
 		}
 
 		/// <summary>
-		/// Process raw data line into the ratings dictionary
+		/// Process raw data line into the ratings dictionary.
 		/// </summary>
-		/// <param name="rawLine">Raw data line</param>
+		/// <param name="rawLine">Raw data line.</param>
 		private void AddRatingTodictionary(string rawLine)
 		{
 			// Split into words
@@ -288,17 +296,22 @@ namespace IMDBDatabase
 		}
 		
 		/// <summary>
-		/// Find the rating data of the requested ID on the dictionary
+		/// Find the rating data of the requested ID on the dictionary.
 		/// </summary>
 		/// <param name="id">Target title ID</param>
 		/// <returns></returns>
 		private Rating GetRatingFromID(int id)
 		{
+			// If title doesn't have a rating give a new default rating
+			if (!_ratingDict.ContainsKey(id)) return new Rating(0,0.0f);
+
+			// Return respective rating
 			return _ratingDict[id];
+
 		}
 
 		/// <summary>
-		/// Extracts int ID from raw data 
+		/// Extracts int ID from raw data .
 		/// </summary>
 		/// <param name="rawTitleId">raw title ID data </param>
 		/// <returns>ID as an int</returns>
